0bf00482b1fee0b1f7afafea5f4864aa3cdab7e6
varshika-gurudu <varshika402@gmail.com>
2018-11-18 21:17:31 -0800

    version changed

--- restifsc/settings.py
+++ restifsc/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import dj_database_url
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -23,9 +24,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'kfbflf-w$hrqvg5@0&d@s478z#eit*35ns%oa8@iyn_b5lfbww'
-
+#SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
